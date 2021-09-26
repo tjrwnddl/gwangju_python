9f4a2831be469dda50b010e5c4d8ea39359877d2
songs <tjrwnddl@naver.com>
2021-09-26 19:02:04 +0900

    second edit

---
+++ hello.py
@@ -0,0 +1,8 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+# 늘하던거라... 뭐 괜찮지 않아?
+print("helloworld 헬로월드")
+print("hello")
