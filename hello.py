ac2b8f7b71c9fb71d7920f03f7b56e7875f63a4f
song <tjrwnddl@naver.com>
2021-10-08 15:50:00 +0900

    헬로월드를 헬로 월드로

--- hello.py
+++ hello.py
@@ -4,5 +4,5 @@ sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 # 늘하던거라... 뭐 괜찮지 않아?
-print("helloworld 헬로월드")
+print("helloworld 헬로 월드")
 print("hello")
